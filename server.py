1ce89872b7ce2529f952f2c7f9416793a77fcfa1
Thomas Seitz <thomas@example.com>
2026-01-08 00:22:14 +0100

    Configure server to run on port 3000 with unsafe werkzeug enabled

--- server.py
+++ server.py
@@ -159,4 +159,4 @@ def on_send_message(data):
         emit('public_message', {'from': username, 'message': message}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, port=3000)
